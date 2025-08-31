4dd0f984bfb792a5f39a4dcf7e2671f26da12015
Ahmed Salama <ahmedsalamafcis@gmail.com>
2025-08-31 18:04:22 +0300

    image search engine app start

---
+++ Image Search Engine/script.js
@@ -0,0 +1,4 @@
+searchForm = document.getElementById("search-form");
+searchBox = document.getElementById("search-box");
+searchResult = document.getElementById("search-result");
+showMoreBtn = document.getElementById("show-more-btn");
